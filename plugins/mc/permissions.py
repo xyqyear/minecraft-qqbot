599cf9111a88aecb88adade5ed4ef75d2a189054
xyqyear <215908558@qq.com>
2020-07-10 23:56:48 +0800

    no change made

--- plugins/mc/permissions.py
+++ plugins/mc/permissions.py
@@ -8,19 +8,6 @@ class PermissionManager:
         self.user_permissions = dict()
         self.servers = []
 
-    def put_permission(self, node_dict, node_permission):
-        if len(node_permission) > 2:
-            if node_permission[0] not in node_dict:
-                node_dict[node_permission[0]] = dict()
-            self.put_permission(node_dict[node_permission[0]], node_permission[1:])
-
-        else:
-            if node_permission[0] not in node_dict:
-                node_dict[node_permission[0]] = list()
-
-            if len(node_permission) > 1:
-                node_dict[node_permission[0]].append(node_permission[1])
-
     def load_user_permissions(self, config_user_permissions: dict):
         # handling group permissions
         self.user_permissions['group'] = dict()
@@ -105,6 +92,19 @@ class PermissionManager:
 
     def add_server(self, server_name):
         self.servers.append(server_name)
+
+    def put_permission(self, node_dict, node_permission):
+        if len(node_permission) > 2:
+            if node_permission[0] not in node_dict:
+                node_dict[node_permission[0]] = dict()
+            self.put_permission(node_dict[node_permission[0]], node_permission[1:])
+
+        else:
+            if node_permission[0] not in node_dict:
+                node_dict[node_permission[0]] = list()
+
+            if len(node_permission) > 1:
+                node_dict[node_permission[0]].append(node_permission[1])
 
     def register(self, perm):
         self.put_permission(self.all_permissions, perm.split('.'))
