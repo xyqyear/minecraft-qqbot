062e46acedd1a0bff0b68b8867ed68596fd17313
xyqyear <215908558@qq.com>
2020-07-17 23:50:14 +0800

    add function to read new log and add test for it

--- tests/test_file_utils.py
+++ tests/test_file_utils.py
@@ -1,30 +1,63 @@
 import asyncio
+import pytest
 import os
 
-from utils.file_utils import write_file, read_file, async_file_exist
+from utils.file_utils import async_write_file, async_read_file, async_file_exist, async_get_new_content
 
 
-def test_write_file():
+@pytest.mark.asyncio
+async def test_write_file():
     test_file_name = 'test_file'
     test_file_content = 'random_content'
-    asyncio.run(write_file(test_file_name, test_file_content))
+    await async_write_file(test_file_name, test_file_content)
     with open(test_file_name) as f:
         assert f.read() == test_file_content
     os.remove(test_file_name)
 
 
-def test_read_file():
+@pytest.mark.asyncio
+async def test_read_file():
     test_file_name = 'test_file'
     test_file_content = 'random_content'
     with open(test_file_name, 'w') as f:
         f.write(test_file_content)
-    assert asyncio.run(read_file(test_file_name)) == test_file_content
+    assert (await async_read_file(test_file_name)) == test_file_content
     os.remove(test_file_name)
 
 
-def test_async_file_exist():
+@pytest.mark.asyncio
+async def test_async_file_exist():
     test_file_name = 'test_file0'
     with open(test_file_name, 'w') as f:
         pass
-    assert asyncio.run(async_file_exist(test_file_name))
+    assert (await async_file_exist(test_file_name))
     os.remove(test_file_name)
+
+
+@pytest.mark.asyncio
+async def test_async_get_new_content():
+    test_filename = 'test.log'
+    with open(test_filename, 'w') as f:
+        f.write('1234')
+
+    assert (await async_get_new_content(test_filename)) == ''
+
+    with open(test_filename, 'a') as f:
+        f.write('5678')
+
+    assert (await async_get_new_content(test_filename)) == '5678'
+    assert (await async_get_new_content(test_filename)) == ''
+
+    with open(test_filename, 'w') as f:
+        f.write('90')
+
+    assert (await async_get_new_content(test_filename)) == '90'
+    assert (await async_get_new_content(test_filename)) == ''
+
+    with open(test_filename, 'a') as f:
+        f.write('8765')
+
+    assert (await async_get_new_content(test_filename)) == '8765'
+    assert (await async_get_new_content(test_filename)) == ''
+
+    os.remove(test_filename)
