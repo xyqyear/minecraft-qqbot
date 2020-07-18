78d87f1bb199b74932e99d8deb17043ea89a9152
xyqyear <215908558@qq.com>
2020-07-18 10:37:35 +0800

    add function that allow players send message from the server to group chat

---
+++ nonebot_plugins/server2group.py
@@ -0,0 +1,26 @@
+import nonebot
+from aiocqhttp.exceptions import Error as CQHttpError
+
+from config import DEFAULT_GROUP, LOG_PATH, DEFAULT_SERVER
+from utils.file_utils import async_get_new_content
+from utils.mc_utils import parse_logs, send_command
+
+
+@nonebot.scheduler.scheduled_job('interval', seconds=1)
+async def _():
+    new_logs = await async_get_new_content(LOG_PATH)
+    if not new_logs:
+        return
+
+    new_messages = parse_logs(new_logs)
+    if not new_messages:
+        return
+
+    bot = nonebot.get_bot()
+    for name, message in new_messages:
+        print(f'<{name}> {message}')
+        try:
+            await bot.send_group_msg(group_id=DEFAULT_GROUP,
+                                     message=f'<{name}> {message}')
+        except CQHttpError:
+            await send_command(DEFAULT_SERVER, f'message "{message}" failed to send')
--- tests/test_request_utils.py
+++ tests/test_request_utils.py
@@ -1,6 +1,6 @@
 from utils.request_utils import uuid2name
 import os
-import json
+import ujson
 import asyncio
 
 
@@ -12,12 +12,12 @@ def test_uuid2name():
     test_cache_file = 'test_cache_json_file'
     assert asyncio.run(uuid2name(test_uuid0, test_cache_file)) == test_name0
     with open(test_cache_file) as f:
-        assert f.read() == json.dumps({test_uuid0: test_name0})
+        assert f.read() == ujson.dumps({test_uuid0: test_name0})
     assert asyncio.run(uuid2name(test_uuid0, test_cache_file)) == test_name0
 
     assert asyncio.run(uuid2name(test_uuid1, test_cache_file)) == test_name1
     with open(test_cache_file) as f:
-        assert f.read() == json.dumps({test_uuid0: test_name0, test_uuid1: test_name1})
+        assert f.read() == ujson.dumps({test_uuid0: test_name0, test_uuid1: test_name1})
     assert asyncio.run(uuid2name(test_uuid0, test_cache_file)) == test_name0
     assert asyncio.run(uuid2name(test_uuid1, test_cache_file)) == test_name1
     os.remove(test_cache_file)
