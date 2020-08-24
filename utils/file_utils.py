173214f408cf6854ce74f88ddb7415c29a879745
xyqyear <215908558@qq.com>
2020-08-24 20:05:39 +0800

    feat(network_utils): add some netwoek utils; refactor(file_utils): minor improvement

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,23 +1,51 @@
+import os
 import asyncio
 import aiofiles
 
-from aiofiles import os as async_os
+from typing import Union
+
+from utils.aio_utils import to_async
 
 file_locks = dict()
 file_pointer = dict()
 
 
-async def async_write_file(filename: str, file_content: str):
-    if filename not in file_locks:
-        file_locks[filename] = False
-    if not file_locks[filename]:
-        file_locks[filename] = True
-        async with aiofiles.open(filename, 'w') as file:
+@to_async
+def async_file_exist(filename: str):
+    return os.path.exists(filename)
+
+
+@to_async
+def async_file_size(filename: str):
+    return os.path.getsize(filename)
+
+
+@to_async
+def async_mkdir(path: str):
+    os.mkdir(path)
+
+
+async def async_write_file(file_path: str, file_content: Union[str, bytes], mode: str = None):
+    """
+    write content to file asynchronously
+    :param file_path: filename
+    :param file_content: file's content
+    :param mode: '' for 'w', 'b' for 'wb'
+    """
+    path, filename = os.path.split(file_path)
+    if path and not await async_file_exist(path):
+        await async_mkdir(path)
+
+    if file_path not in file_locks:
+        file_locks[file_path] = False
+    if not file_locks[file_path]:
+        file_locks[file_path] = True
+        async with aiofiles.open(file_path, 'wb' if mode == 'b' else 'w') as file:
             await file.write(file_content)
-        file_locks[filename] = False
+        file_locks[file_path] = False
     else:
         await asyncio.sleep(0.01)
-        await async_write_file(filename, file_content)
+        await async_write_file(file_path, file_content)
 
 
 async def async_read_file(filename: str, seek=0, size=-1) -> str:
@@ -25,19 +53,6 @@ async def async_read_file(filename: str, seek=0, size=-1) -> str:
         if seek:
             await file.seek(seek)
         return await file.read(size)
-
-
-async def async_file_exist(filename: str) -> bool:
-    try:
-        async with aiofiles.open(filename) as f:
-            pass
-    except FileNotFoundError:
-        return False
-    return True
-
-
-async def async_file_size(filename: str) -> int:
-    return (await async_os.stat(filename)).st_size
 
 
 async def async_get_new_content(filename: str) -> str:
--- utils/network_utils.py
+++ utils/network_utils.py
@@ -1,4 +1,7 @@
 import socket
+import aiohttp
+
+download_session = None
 
 
 def is_ipv4(address: str) -> bool:
@@ -7,3 +10,27 @@ def is_ipv4(address: str) -> bool:
         return True
     except socket.error:
         return False
+
+
+async def get_session():
+    global download_session
+    if download_session is None:
+        download_session = aiohttp.ClientSession()
+    return download_session
+
+
+async def async_download_bytes(url: str, retry_count: int = 3, timeout: int = 3) -> bytes:
+    session = await get_session()
+
+    while retry_count:
+        try:
+            async with session.get(url, timeout=aiohttp.ClientTimeout(total=timeout)) as resp:
+                if resp.status == 200:
+                    return await resp.read()
+                else:
+                    retry_count -= 1
+                    continue
+        except:
+            retry_count -= 1
+
+    return b''
