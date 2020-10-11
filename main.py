36317b1e6eb677c4618dd91b4c260709eedae50f
xyqyear <215908558@qq.com>
2020-10-11 22:52:03 +0800

    change logging level to WARNING to get rid of the useless debug messages

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import logging
 if __name__ == '__main__':
     config.load()
     bot.init()
-    logging.getLogger('apscheduler.executors.default').setLevel(logging.WARNING)
+    logging.getLogger().setLevel(logging.WARNING)
 
     from bot_plugins import mc, server2group, repeat
 
